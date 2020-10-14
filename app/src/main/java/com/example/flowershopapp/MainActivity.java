f7ea0855a33c851b21908b0bf70e8df49ed7ac0b
Amadea Noemi Pal <amadeanoemi@Amadeas-MacBook-Pro.local>
2020-10-14 22:19:58 +0100

    create 2 buttons

--- app/src/main/java/com/example/flowershopapp/MainActivity.java
+++ app/src/main/java/com/example/flowershopapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.flowershopapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    //A method that opens the till
+    public void openTill (View view) {
+
+    }
+
+
+
+
+    //A method that opens the Stock Management
+    public void openStockManagement(View view) {
     }
 }
---
+++ app/src/main/java/com/example/flowershopapp/StockManagement.java
@@ -0,0 +1,4 @@
+package com.example.flowershopapp;
+
+public class StockManagement {
+}
